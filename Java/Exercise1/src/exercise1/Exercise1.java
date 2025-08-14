05bf9cdf633ec79fbf3766042dad97ccfd794246
NoahNguyen0709 <nguyenhoangv1384@gmail.com>
2025-08-14 21:32:06 +0700

    Add files via upload

---
+++ Java/Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Exercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x1 = 1 + 2;
+        System.out.println("x1 = " + x1);
+        double x2 = 3.5 + 8;
+        System.out.println("x2 = " + x2);
+        double x3 = 1.0/2;
+        System.out.println("x3 = " + x3);
+        double x4 = 4.5 + 7;
+        System.out.println("x4 = " + x4);
+        int x5 = 4*5 + 1;
+        System.out.println("x5 = " + x5);
+        int x6 = 20%3 + 2 - 7;
+        System.out.println("x6 = " + x6);
+    }
+    
+}
